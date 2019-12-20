7da7ff7ae169f9a308314630518cb8ff6ed50562
SPrio-kt <supriyo.mahanta@kreeti.com>
2019-12-20 12:11:49 +0530

    /metrices loaded

--- config/config.exs
+++ config/config.exs
@@ -15,7 +15,8 @@ config :elixir_monitoring_prom, ElixirMonitoringPromWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "kyubJ0Ab0byZw57MkYHN/7s8euCk6tn6gwHAmQ5C6SofxU/I4E3SvHhTW1nwBnic",
   render_errors: [view: ElixirMonitoringPromWeb.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: ElixirMonitoringProm.PubSub, adapter: Phoenix.PubSub.PG2]
+  pubsub: [name: ElixirMonitoringProm.PubSub, adapter: Phoenix.PubSub.PG2],
+  instrumenters: [ElixirMonitoringProm.PhoenixInstrumenter]
 
 # Configures Elixir's Logger
 config :logger, :console,
@@ -24,6 +25,25 @@ config :logger, :console,
 
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
+
+config :prometheus, ElixirMonitoringProm.PipelineInstrumenter,
+  labels: [:status_class, :method, :host, :scheme, :request_path],
+  duration_buckets: [
+    10,
+    100,
+    1_000,
+    10_000,
+    100_000,
+    300_000,
+    500_000,
+    750_000,
+    1_000_000,
+    1_500_000,
+    2_000_000,
+    3_000_000
+  ],
+  registry: :default,
+  duration_unit: :microseconds
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
